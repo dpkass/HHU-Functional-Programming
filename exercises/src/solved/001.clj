f332da2c704364842514156d0232a31859a07f4e
Taha El Amine Kassabi <taha.kassabi@gmail.com>
2023-11-14 15:14:51 +0100

    L1 - Intro

---
+++ exercises/src/solved/001.clj
@@ -0,0 +1,11 @@
+(ns solved.001)
+
+(comment "Nothing but the Truth")
+
+
+(comment "Complete the expression so it will evaluate to true.")
+
+
+(def solution nil)
+
+(assert (and (= solution nil)))
---
+++ exercises/src/solved/002.clj
@@ -0,0 +1,9 @@
+(ns solved.002)
+
+(comment "Simple Math")
+
+(comment "Innermost forms are evaluated first.")
+
+(def solution 4)
+
+(assert (and (= (- 10 (* 2 3)) solution)))
