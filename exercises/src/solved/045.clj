543373c7a18010878dfdf2b015938d6a5d22eac8
Taha El Amine Kassabi <taha.kassabi@gmail.com>
2023-11-14 15:18:05 +0100

    L3 - HOF Intro: Find solution

---
+++ exercises/src/solved/045.clj
@@ -0,0 +1,12 @@
+(ns solved.045)
+
+(comment "Intro to Iterate")
+
+
+(comment "The iterate function can be used to produce
+an infinite lazy sequence.")
+
+
+(def solution '(1 4 7 10 13))
+
+(assert (and (= solution (take 5 (iterate (fn* [p1__4821#] (+ 3 p1__4821#)) 1)))))
