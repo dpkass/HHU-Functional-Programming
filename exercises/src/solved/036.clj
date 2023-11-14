af316f6e3845f88938beb07b66d9d054ea0943bf
Taha El Amine Kassabi <taha.kassabi@gmail.com>
2023-11-14 15:23:12 +0100

    L2 - Bonus

---
+++ exercises/src/solved/036.clj
@@ -0,0 +1,11 @@
+(ns solved.036)
+
+(comment "Let it Be")
+
+(comment "Can you bind x, y, and z so that these are all true?")
+
+
+;; solution should be the same in all three instances
+(assert (and (= 10 (let [x 7 y 3 z 1] (+ x y)))
+             (= 4 (let [x 7 y 3 z 1] (+ y z)))
+             (= 1 (let [x 7 y 3 z 1] z))))
