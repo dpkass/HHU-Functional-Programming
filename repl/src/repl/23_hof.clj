b2408d4fcbe7e7b944b6d892bfea8a880696239c
Taha El Amine Kassabi <taha.kassabi@gmail.com>
2023-11-19 10:18:46 +0100

    L3 - REPL Session - HOF

--- repl/src/repl/23_hof.clj
+++ repl/src/repl/23_hof.clj
@@ -1,14 +1,18 @@
 (ns repl.23-hof)
 
-(comment 
+;; add max prints for repl
+(set! *print-length* 50)
+(set! *print-level* 5)
+
+
 ;; reminder:
 
-  (defn ! [n]
-    (if (= 1 n)
-      n
-      (*' n (! (dec n))))) ;; Solution: use *' for auto promotion
+(defn ! [n]
+  (if (= 1 n)
+    n
+    (*' n (! (dec n)))))                                    ;; Solution: use *' for auto promotion
 
-  (! 10000)
+(! 10000)
 
 ;; Recursive calls produce frames on the stack and can cause a stack overflow
 ;; A stack frame contains local variables and is needed
@@ -30,15 +34,15 @@
 ;; HOF are not special, since functions are not special (first-class citizens)
 
 ;; fn as parameter
-  (defn evaluate1 [f v] (f v))
-  (evaluate1 inc 12)
+(defn evaluate1 [f v] (f v))
+(evaluate1 inc 12)
 
 ;; fn as value
-  (defn mk-adder [n] (fn [x] (+ x n)))
+(defn mk-adder [n] (fn [x] (+ x n)))
 
 ;; effectively: (def foo (fn [x] (+ x 17))) -- beta reduction!
-  (def foo (mk-adder 17))
-  (foo 4)
+(def foo (mk-adder 17))
+(foo 4)
 
 ;; Constructs, that occasionally happen:
 ;; ((mk-adder 17) 3)
@@ -53,39 +57,39 @@
 
 
 ;; map accepts a function and a sequence of values x1, ..., xn and returns the sequence f(x1), ..., f(xn)
-  (map inc [2 3 4 5 6])
+(map inc [2 3 4 5 6])
 ;; If multiple lists are provided, then the n-ary function is called with one element from each list until (at least) one input-list is exhausted
-  (map + [1 2 3] [3 4 5] [2 3])
+(map + [1 2 3] [3 4 5] [2 3])
 
 
 ;; You can use any function
 ;; This produces all even numbers for example
-  (map (fn [x] (* 2 x)) (range))
+(map (fn [x] (* 2 x)) (range))
 
 
 
 ;; 'map' is lazy
 
 ;; the side effect (the print) is used for didactic reasons
-  (def squares
-    (map (fn [x] (println :berechnet x) (* x x)) (range)))
+(def squares
+  (map (fn [x] (println :berechnet x) (* x x)) (range)))
 
 ;; the side effect only occurs once during the first evaluation
-  (first squares)
-  (nth squares 33)
+(first squares)
+(nth squares 33)
 ;; already occurred side effects do not happen again
-  (nth squares 39)
+(nth squares 39)
 
 ;; (!!) It is an extraordinarily stupid idea to combine side effects with laziness
 ;; It is often difficult to predict when and if a side effect occurs
 
 ;; Example:
-  (defn my-debug-print [input]
-    (map println input)
-    nil) ;; no return value that is reasonable
+(defn my-debug-print [input]
+  (map println input)
+  nil)                                                      ;; no return value that is reasonable
 
 ;; Whoops!
-  (my-debug-print [1 2 3])
+(my-debug-print [1 2 3])
 
 ;; there are even more tricky cases, where calling a function in the REPL causes the side effect to trigger (during the print phase),
 ;; but during the execution of a real program  it is not...
@@ -94,25 +98,30 @@
 ;; Other constructs exist for use with side effects, e.g. 'doseq'
 ;; it guarantees the evaluation of all elements and associated side effects:
 
-  (do
-    (doseq [x [1 2 3]]
-      (println x))
-    nil)
+(do
+  (doseq [x [1 2 3]]
+    (println x))
+  nil)
 
 ;; map always returns a LazySeq.
 ;; A type-preserving map could be defined as
-  (defn mm [f v] (into (empty v) (map f v)))
+(defn mm [f v] (into (empty v) (map f v)))
 
 
+; added from intro (possibly simply misplaced)
+(mm inc [1 2 3])
+(mm inc '(1 2 3))
+(mm inc #{1 2 4})
 
 ;; 2. filter (and remove)
 
 ;; 'filter' expects a function p? and a sequence x1, ..., xn.
 ;; a sequence of all xi, for which (p? xi) returns a truthy value, is produced
-  (filter (fn [x] (< 2 x 6)) [1 2 3 4 5 6])
+(filter (fn [x] (< 2 x 6)) [1 2 3 4 5 6])
 
 ;; What does 'remove' do?
-  (remove (fn [x] (< 2 x 6)) [1 2 3 4 5 6])
+; --> opposite of filter
+(remove (fn [x] (< 2 x 6)) [1 2 3 4 5 6])
 
 
 
@@ -126,17 +135,17 @@
 ;; the call (reduce f a [x y z]) calculates
 ;; (f (f (f (f a) x) y) z)
 
-  (reduce + 1 [2 3 4 5 6]) ;; (+  (+ (+ 1 2) 3) 4 ...)
+(reduce + 1 [2 3 4 5 6])                                    ;; (+  (+ (+ 1 2) 3) 4 ...)
 
 ;; (reduce reduce-function init-value sequence)
 
-  (reduce * (range 2 7))
+(reduce * (range 2 7))
 
-  (reduce + 0 (range 2 7))
-  (reduce + (range 2 7))
+(reduce + 0 (range 2 7))
+(reduce + (range 2 7))
 
-  (reduce conj [] [1 2 3])
-  (reduce conj '() [1 2 3])
+(reduce conj [] [1 2 3])
+(reduce conj '() [1 2 3])
 
 
 ;; reduce handles some special cases differently
@@ -144,24 +153,26 @@
 ;; - the initial value is left out?
 ;; - the collection is empty?
 ;; - both of the above cases apply at the same time?
-
+(reduce conj [1 2 3])
+(reduce conj [1 2 3] [])
+(reduce conj [])
 
 
 ;; now that we know reduce, we can fix our factorial function:
-
-  (defn ! [n]
-    (reduce *' 0 (range 1 (inc n))))
+; wrong
+(defn ! [n]
+  (reduce *' 1 (range 1 (inc n))))
 
 
 ;; 4. apply (already seen - unpacking of last argument):
 (apply str (interpose \newline [1 2 3]))
 
-;; 5. partial:
+ ;; 5. partial:
 ;; partial is function that takes another function and "hard-wires" its first argument(s).
 ;; it returns a function that may take more arguments:
 
-(partial + 1 2) ;; a function, that always adds 3
-((partial + 1 2) 3 4) ;; 1 + 2 (hard-wired) + 3 + 4
+(partial + 1 2)                                             ;; a function, that always adds 3
+((partial + 1 2) 3 4)                                       ;; 1 + 2 (hard-wired) + 3 + 4
 
 
 ;; 6. iterate:
@@ -181,20 +192,20 @@
 ;; some more examples of HOFs:
 ;; note: keywords can act as functions -
 ;; we do not consider functions that may return a keyword (e.g., first) as higher-order function per se
-  (group-by :lecturer
-            [{:course :fp, :year 2013, :lecturer :bendisposto}
-             {:course :fp, :year 2021, :lecturer :koerner}
-             {:course :propra, :year 2020, :lecturer :bendisposto}])
+(group-by :lecturer
+          [{:course :fp, :year 2013, :lecturer :bendisposto}
+           {:course :fp, :year 2021, :lecturer :koerner}
+           {:course :propra, :year 2020, :lecturer :bendisposto}])
 
 
-  (merge-with + {:a 1, :b 2} {:a 3})
+(merge-with + {:a 1, :b 2} {:a 3})
 
-  (take-while neg? (range -10 10000))
-  (drop-while neg? (range -10 10))
+(take-while neg? (range -10 10000))
+(drop-while neg? (range -10 10))
 
 ;; once you know what you are doing,
 ;; nesting of HOFs may get crazy (from one of my projects):
-   ; (reduce (partial merge-with concat) (map (partial analyze-block property-map list) data))  
+; (reduce (partial merge-with concat) (map (partial analyze-block property-map list) data))
 
 
 ;; Remark.
@@ -204,10 +215,46 @@
 ;; As an exercise, try implementing some list operations by reducing a function!
 ;; for example: first, map, filter, concat, interpose, take, ...
 
+; works if first element isn't :none...
+(defn first [coll]
+  (reduce #(if (= %1 :none) %2 %1) :none coll))
+
+(first [1 2 3])
+
+
+(defn map [f coll]
+  (reduce #(cons (f %2) %1) (empty coll) (reverse coll)))
+
+(map inc (range 10))
+
+
+(defn filter [f coll]
+  (reduce #(if (f %2) (cons %2 %1) %1) (empty coll) (reverse coll)))
+
+(filter #(< 4 %) (range 10))
+
+
+(defn concat [coll1 coll2]
+  (reduce #(conj %1 %2) (vec coll1) coll2))
+
+(concat '(1 2 3) '(4 5 6))
+
+
+(defn interpose [x coll]
+  (reduce #(conj %1 %2 x) [] coll))
+
+(interpose :abc [:a :b :c])
+
+
+(defn take [n coll]
+  (reduce #(if (< (count %1) n)
+             (conj %1 %2)
+             %1)
+          []
+          coll))
+
+(take 3 '(1 2 3 4 5))
+
 
 ;; also: try to implement naive versions of basic higher-order functions,
 ;; e.g., map, filter, reduce, apply, partial
-
-
-
-)
