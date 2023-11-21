a12390fb9d001cd99ed7c0c33c0c71a6a2bae025
Taha El Amine Kassabi <taha.kassabi@gmail.com>
2023-11-21 18:27:31 +0100

    Upgrade to Clojure 1.11

--- repl/project.clj
+++ repl/project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
 
                  ;; 05 Rekursion
                  [org.clojure/tools.trace "0.7.9"]
