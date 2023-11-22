afbc18bc2781310f13d7427dd2da670a532482ee
Taha El Amine Kassabi <taha.kassabi@gmail.com>
2023-11-22 08:11:00 +0100

    L5 - Apply Newton's Method to get sqrt

--- src/l5/e5_2.clj
+++ src/l5/e5_2.clj
@@ -1,5 +1,6 @@
 (ns l5.e5-2)
 
+
 (comment "a) Write a function (defn newton [f f’] ...) that receives the function and its
 derivative as a parameter. The call to newton should then return a function which in turn
 receives a starting value x and a precision eps and approximates the solution for f (x) = 0 ± eps
@@ -21,9 +22,30 @@ receives a starting value x and a precision eps and approximates the solution fo
 
 
 
-(defn is-close [a b]
-  (< (diff a b) 1e-9))
+(defn is-close
+  ([a b]
+   (is-close a b 1e-9))
+  ([a b abs-diff]
+   (< (diff a b) abs-diff)))
 
-(is-close ((newton #(* % %) #(* 2 %)) 1 1e-10) 0)      ;x^2
-(is-close ((newton #(* % % %) #(* 3 (* % %))) -1 1e-10) 0) ; x^3
-(is-close ((newton #(Math/pow (- % 2) 2) #(* 2 (- % 2))) 20 1e-10) 2) ; (x-2)^2
+(is-close ((newton #(* % %) #(* 2 %)) 1 1e-10) 0)           ;x^2
+(is-close ((newton #(* % % %) #(* 3 (* % %))) -1 1e-10) 0)  ; x^3
+(is-close ((newton #(- (* % %) 2) #(* 2 %)) 1 1e-5) (Math/sqrt 2) 1e-6) ;x^2-2
+(is-close ((newton #(Math/pow (- % 2) 2) #(* 2 (- % 2))) 20 1e-5) 2 1e-6) ; (x-2)^2
+
+
+
+(comment "b) The square root of a number K is a solution for the equation c2 − K = 0. So we can
+apply Newton’s Method to the function f(x) = x2 − K. Write a function (defn sqrt [n] ...) which
+approximates the square root of n using Newton’s method. You can use 10−5 as both initial value
+and precision.")
+
+(defn sqrt [n]
+  ((newton #(- (* % %) n) #(* 2 %)) 1 1e-10))
+
+(is-close (sqrt 0) 0)
+(is-close (sqrt 1) 1)
+(is-close (sqrt 2) (Math/sqrt 2))
+(is-close (sqrt 4) 2)
+(is-close (sqrt 9) 3)
+(is-close (sqrt 1000000000000000000000000000000000000000000000000) 1000000000000000000000000)
