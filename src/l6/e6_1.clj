50daf346410ee0abd4f0bcc973e275b8f152c02c
Taha El Amine Kassabi <taha.kassabi@gmail.com>
2023-11-30 22:21:43 +0100

    L6 - E1 - Write Newton's Method using Fixedpoint function

--- src/l6/e6_1.clj
+++ src/l6/e6_1.clj
@@ -12,3 +12,25 @@ sufficiently well.")
     (if (eps? xn xn+1)
       xn+1
       (recur xn+1 (F xn+1)))))
+
+
+(comment "b) Write Newton’s method (from as instance of this fixed point function.")
+
+(defn newton [f f']
+  (fn [guess eps]
+    (fixedpoint
+     (fn newton-seq [x] (- x (/ (f x) (f' x))))
+     guess
+     #(< (abs (- % %2)) eps))))
+
+
+(defn is-close
+  ([a b]
+   (is-close a b 1e-9))
+  ([a b abs-diff]
+   (< (abs (- a b)) abs-diff)))
+
+(is-close ((newton #(* % %) #(* 2 %)) 1 1e-10) 0)             ;x^2
+(is-close ((newton #(* % % %) #(* 3 (* % %))) -1 1e-10) 0)    ; x^3
+(is-close ((newton #(- (* % %) 2) #(* 2 %)) 1 1e-6) (Math/sqrt 2) 1e-5) ;x^2-2
+(is-close ((newton #(Math/pow (- % 2) 2) #(* 2 (- % 2))) 20 1e-6) 2 1e-5) ; (x-2)^2
